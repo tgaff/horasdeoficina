494586ea09c3bba2017f4d32b63e39b4352afdf6
tgaff <tgaff@alumni.nd.edu>
2015-07-29 09:38:51 -0700

    Prevent WeeklyTimeBlocks from overlapping

--- spec/models/weekly_time_block_spec.rb
+++ spec/models/weekly_time_block_spec.rb
@@ -24,4 +24,45 @@ RSpec.describe WeeklyTimeBlock, type: :model do
   it 'doesnt permit days of the week from other planets' do
     expect(FactoryGirl.build(:weekly_time_block, dotw: 'Towelday') ).to be_invalid
   end
+
+  describe 'participant with multiple time blocks' do
+    let(:participant) { FactoryGirl.create(:class_participant) }
+    before do
+      FactoryGirl.create(:weekly_time_block, class_participant: participant,
+                        from: '08:00', to: '13:00')
+      FactoryGirl.create(:weekly_time_block, class_participant: participant,
+                        from: '17:00', to: '22:00')
+    end
+
+    it 'cannot create a time-block that starts during another' do
+      new_tb = FactoryGirl.build(:weekly_time_block, class_participant: participant,
+                        from: '12:00', to: '15:00')
+      expect(new_tb).to be_invalid
+    end
+
+    it 'cannot create a time-block that ends during another' do
+      new_tb = FactoryGirl.build(:weekly_time_block, class_participant: participant,
+                        from: '14:00', to: '18:00')
+      expect(new_tb).to be_invalid
+    end
+
+    it 'can create a time-block that abuts others' do
+      new_tb = FactoryGirl.build(:weekly_time_block, class_participant: participant,
+                        from: '13:00', to: '17:00')
+      expect(new_tb).to be_valid
+    end
+
+    it 'can create a time-block that overlaps others if not on the same day' do
+      new_tb = FactoryGirl.build(:weekly_time_block, class_participant: participant,
+                        from: '9:00', to: '18:00', dotw: 'Tuesday')
+      expect(new_tb).to be_valid
+    end
+
+    it 'cannot create a time-block that completely contains another' do
+      new_tb = FactoryGirl.build(:weekly_time_block, class_participant: participant,
+                        from: '07:00', to: '23:00')
+      expect(new_tb).to be_invalid
+    end
+
+  end
 end
