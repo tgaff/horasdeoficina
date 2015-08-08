602c3852ffce65310900b73c94468a5cb9237472
tgaff <tgaff@alumni.nd.edu>
2015-08-08 14:27:08 -0700

    Update view to ignore timezones so that our times are just times wherever

--- spec/features/class_participants_spec.rb
+++ spec/features/class_participants_spec.rb
@@ -9,9 +9,11 @@ RSpec.feature "Class Participants",
   :js do
 
 
-  background do
-    cp = FactoryGirl.create(:class_participant)
-    FactoryGirl.create(:weekly_time_block, class_participant: cp)
+  let!(:cp) { FactoryGirl.create(:class_participant) }
+  let!(:wtb) { FactoryGirl.create(:weekly_time_block, class_participant: cp) }
+
+  def page_time_format(dt)
+    dt.strftime('%l:%M').strip
   end
 
   scenario 'the calendar is displayed in week view' do
@@ -20,4 +22,10 @@ RSpec.feature "Class Participants",
     expect(page.find('#calendar')).to have_css('.fc-day-header', count: 7)
   end
 
+  scenario 'items on the calendar are visible' do
+    visit class_participants_path
+    # #calendar > div.fc-view-container > div > table > tbody > tr > td > div.fc-time-grid-container.fc-scroller > div > div.fc-content-skeleton > table > tbody > tr > td:nth-child(4) > div > a.fc-time-grid-event.fc-v-event.fc-event.fc-start.fc-not-end.fc-draggable
+    # fc-event-container .fc-content .fc-time / .fc-title
+    expect(page).to have_css(".fc-event-container .fc-time[data-start='#{page_time_format(wtb.from)}']")
+  end
 end
