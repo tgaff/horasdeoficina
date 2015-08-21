bdcdea756fda270ef59fad8b481f3b28d8f43744
tgaff <tgaff@alumni.nd.edu>
2015-08-21 13:17:00 -0700

    role has shortcuts for student and educator

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -13,4 +13,24 @@ RSpec.describe Role, type: :model do
     role.role_name = 'AbCd'
     expect{role.save}.to change{role.role_name}.from('AbCd').to('abcd')
   end
+
+  describe '::educator' do
+    before { FactoryGirl.create :educator }
+    it 'returns an instace of the model' do
+      expect(Role::educator).to be_a Role
+    end
+    it 'finds the "student" role' do
+      expect(Role::educator.role_name).to eq 'educator'
+    end
+  end
+
+  describe '::student' do
+    before { FactoryGirl.create :student_role }
+    it 'returns an instace of the model' do
+      expect(Role::student).to be_a Role
+    end
+    it 'finds the "student" role' do
+      expect(Role::student.role_name).to eq 'student'
+    end
+  end
 end
