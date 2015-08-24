d0570fa658bff3c129870d1da34459c8acb91972
tgaff <tgaff@alumni.nd.edu>
2015-08-23 21:11:25 -0700

    Change courses to split up by role

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,22 @@
 module CoursesHelper
+
+  # not totally convinced this shouldn't be in the Course model
+  # but it does work on multiple models, so for now I'm setting it up as a helper
+  #
+  def as_an_educator(courses)
+    courses.select do |c|
+      c.course_participants.last.role.role_name == 'educator'
+    end
+  end
+  def as_a_student(courses)
+    as_a('student', courses)
+  end
+
+  private
+
+  def as_a(role_name, courses)
+    courses.select do |c|
+      c.course_participants.first.role.role_name == role_name
+    end
+  end
 end
--- spec/views/courses/index.html.erb_spec.rb
+++ spec/views/courses/index.html.erb_spec.rb
@@ -2,18 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "courses/index", type: :view do
   before(:each) do
+    @student_participant = FactoryGirl.create(:student_participant)
+    @educator_participant = FactoryGirl.create(:educator_participant)
     assign(:courses, [
       Course.create!(
-        :title => "Title"
+        :title => "Title",
+        course_participants: [ @student_participant ]
       ),
       Course.create!(
-        :title => "Title"
+        :title => "Title",
+        course_participants: [ @educator_participant ]
       )
     ])
+
+
   end
 
   it "renders a list of courses" do
     render
     assert_select "tr>td", :text => "Title".to_s, :count => 2
+  end
+
+  it 'has a heading for each role' do
+    render
+    expect(rendered).to have_css('h2', text: 'Learning')
+    expect(rendered).to have_css('h2', text: 'Teaching')
+  end
+
+  it "doesn't show the heading or table if the role isn't present" do
+    assign(:courses, [] )
+    expect(rendered).to_not have_content 'Teaching'
   end
 end
