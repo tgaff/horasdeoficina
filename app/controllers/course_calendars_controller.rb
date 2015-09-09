b48faf9fec4e6b2c4becf1a713a4f52e7c099039
tgaff <tgaff@alumni.nd.edu>
2015-09-08 17:09:46 -0700

    calendar controller #show

--- app/controllers/course_calendars_controller.rb
+++ app/controllers/course_calendars_controller.rb
@@ -1,4 +1,12 @@
 class CourseCalendarsController < ApplicationController
   def show
+    course = Course.find(course_id)
+    @course_participants = course.course_participants
+  end
+
+private
+
+  def course_id
+    params.require(:course_id).to_i
   end
 end
--- spec/controllers/course_calendars_controller_spec.rb
+++ spec/controllers/course_calendars_controller_spec.rb
@@ -3,9 +3,25 @@ require 'rails_helper'
 RSpec.describe CourseCalendarsController, type: :controller do
 
   describe "GET #show" do
+    let(:course) do
+      course = FactoryGirl.build_stubbed(:course)
+    end
+    let(:course_participants) do
+      FactoryGirl.build_stubbed_list(:course_participant, 4)
+    end
+    before do
+      allow(Course).to receive(:find).with(2).and_return course
+      allow(course).to receive(:course_participants).and_return course_participants
+    end
+
     it "returns http success" do
       get :show, course_id: 2
       expect(response).to have_http_status(:success)
+    end
+
+    it "assigns @course_participants" do
+      get :show, course_id: 2
+      expect(assigns(:course_participants)).to eq course_participants
     end
   end
 
