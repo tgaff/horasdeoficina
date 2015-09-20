d0cc75333703ebe69c4c6b32504485940e771648
tgaff <tgaff@alumni.nd.edu>
2015-09-19 23:27:27 -0700

    fix js bug caught by feature specs

--- app/assets/javascripts/course_calendars.js
+++ app/assets/javascripts/course_calendars.js
@@ -1,15 +1,20 @@
 
 $(document).ready(function() {
-
-  // mark all events as background
-  for (var i=0; i < WTBS.length; i++) {
-    WTBS[i].rendering = 'background';
-    //WTBS[i].backgroundColor = '#036';
-    WTBS[i].backgroundColor = 'rgba(0,0,255,0.3)';
-
-  };
   // page is now ready, initialize the calendar...
+
+
   if ($('#calendar.calendar-course').length > 0) {
+
+    // mark all events as background
+    for (var i=0; i < WTBS.length; i++) {
+      WTBS[i].rendering = 'background';
+      //WTBS[i].backgroundColor = '#036';
+      WTBS[i].backgroundColor = 'rgba(0,0,255,0.3)';
+    };
+
+
+
+
     $('#calendar').fullCalendar({
       defaultDate: moment("2015-08-03T12:24:05"),
       defaultView: 'agendaWeek',
